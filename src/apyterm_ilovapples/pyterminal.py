18ad4008e54b59bfd773f02cd2a9833f75ccaf31
ilovapples <89090508+ilovapples@users.noreply.github.com>
2023-05-14 18:06:35 +0000

    .

--- src/apyterm_ilovapples/pyterminal.py
+++ src/apyterm_ilovapples/pyterminal.py
@@ -53,10 +53,25 @@ class OpenFiles:
 
 
 if not os.path.isfile('dir.txt'):
-    if getos() == 'Windows':
-        os.system('create_files.bat')
-    else:
-        os.system('bash create_files.sh')
+    # if getos() == 'Windows':
+    #     os.system('create_files.bat')
+    # else:
+    content = """wget https://raw.githubusercontent.com/ilovapples/terminal/main/src/apyterm_ilovapples/cmds.yaml
+touch .users.txt
+echo 'default' > dir.txt
+echo 'echo $(pwd) > dir.txt' > set_dir.sh
+echo 'echo %cd% > dir.txt' > set_dir.bat
+echo -e '#!/usr/bin/env bash\nrm -r data/*\ntouch data/placeholder.txt\necho '\"'\" > .users.txt\necho '\"default'\" > dir.txt' > reset.sh
+echoe -e 'rmdir data /S /Q\nmd data\ntype nul > data/placeholder.txt\ntype nul > .users.txt\ntype '\"default'\" > dir.txt' > reset.bat
+[ -d data ] || mkdir data
+touch data/placeholder.txt
+wget https://raw.githubusercontent.com/ilovapples/terminal/main/README.md
+[ -d default_data ] || mkdir default_data
+echo 'clear' > default_data/.startup
+cp README.md default_data/README.md"""
+    with open('create_files.sh', 'w') as file:
+        file.write(content)
+    os.system('bash create_files.sh')
 file = OpenFiles()
 
 fileLocation_file = file.open('dir.txt', 'r')
